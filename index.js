5e3d63f1f9124025ce09fe72e39318e618172b73
kridi32 <kridi32@outlook.com>
2022-05-05 20:51:01 +0600

    basic node and express setup done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require(express)
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("first data from the server ")
+});
+
+app.listen(port, ()=> {
+    console.log("server started");
+})

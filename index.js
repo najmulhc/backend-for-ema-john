06c4510ba628393c02e80122014384bb0b2ac718
kridi32 <kridi32@outlook.com>
2022-05-06 11:48:20 +0600

    backend setup for the project is done

--- index.js
+++ index.js
@@ -19,10 +19,29 @@ const run = async () => {
     await client.connect();
     const database = client.db("insertDB").collection("products");
     // create a document to insert
-    app.get("/", async(req, res) => {
-      const query = {};
-      const result = await database.find(query).toArray();
+    app.get("/", async (req, res) => {
+      console.log("client is asking for", req.query);
+      const page = parseInt(req.query.page);
+      const pageSize = parseInt(req.query.size);
+    let result;
+    const query = {};
+      if(page || pageSize){
+        result = await database.find(query).skip(page*pageSize).limit(pageSize).toArray();
+      }
+      else{
+        result = await database.find(query).toArray();
+      }
+
+   
+      
       res.send(result);
+    });
+    app.get("/product/count", async (req, res) => {
+      const query = {};
+      const items = database.find(query);
+      const count = await items.count();
+      res.send({ count });
+      console.log("count sent from db");
     });
   } finally {
   }

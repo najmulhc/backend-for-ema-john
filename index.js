e515732e4bfba3fac722b0080e84a69ae8f75080
kridi32 <kridi32@outlook.com>
2022-05-06 10:10:31 +0600

    now data is sent from mongodb database

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://admin:amiadmin@cluster0.fte9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-const run = async() => {
-    try{
-        await client.connect();
-        const database = client.db("insertDB");
-    const haiku = database.collection("haiku");
-    // create a document to insert
-    const doc = {
-      title: "Record of a Shriveled Datum",
-      content: "No bytes, no problem. Just insert a document, in MongoDB",
-    }
-    const result = await haiku.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    }
-    finally{
-
-    }
-}
-run().catch(console.log);
-
-app.get("/", (req, res) => {
-    res.send("first data from the server ")
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-app.listen(port, ()=> {
-    console.log("server started");
-})
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db("insertDB").collection("products");
+    // create a document to insert
+    app.get("/", async(req, res) => {
+      const query = {};
+      const result = await database.find(query).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+};
+run().catch(console.log);
+
+app.listen(port, () => {
+  console.log("server started");
+});
